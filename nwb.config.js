7b22d133f5ff7340759db8ae97e758978653d6ca
Frank Gutierrez <dearfrankg@gmail.com>
2017-04-02 19:08:07 -0700

    config nwd to resolve src and img

--- nwb.config.js
+++ nwb.config.js
@@ -1,3 +1,17 @@
+var path = require('path')
+
 module.exports = {
-  type: 'react-app'
+  type: 'react-app',
+  webpack: {
+    aliases: {
+      // Enable use of 'img/file.png' paths in JavaScript and
+      // "~images/file.png" paths in stylesheets to require an image from
+      // src/images from anywhere in the the app.
+      'img': path.resolve('src/images'),
+      // Enable use of require('src/path/to/module.js') for top-down imports
+      // from anywhere in the app, to promote writing location-independent
+      // code by avoiding ../ directory traversal.
+      'src': path.resolve('src')
+    }
+  }
 }
